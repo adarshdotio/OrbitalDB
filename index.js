d0f8fac7e362e6123fe46838885f95e50036f234
adarshdotio <urstrulyadxrsh@gmail.com>
2026-02-28 00:14:32 +0530

    OrbitalDB Completed v1

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const Collection = require('./collection');
+class OrbitalDB {
+    constructor(dbName) {
+        this.dbDir = path.join(process.cwd(), 'data', dbName);
+        if (!fs.existsSync(this.dbDir)) fs.mkdirSync(this.dbDir, { recursive: true });
+        this.collections = {};
+    }
+    collection(name, schema = {}) {
+        if (!this.collections[name]) this.collections[name] = new Collection(this.dbDir, name, schema, this);
+        return this.collections[name];
+    }
+}
+module.exports = OrbitalDB;
